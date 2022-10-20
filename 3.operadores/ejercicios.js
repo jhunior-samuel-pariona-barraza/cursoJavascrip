ddd22ff73ea8d27cd7255e80e3eed45aa97dc21d
jhunior <19jhuniorsamuel@gmail.com>
2022-10-20 10:02:18 -0500

    TIPOS DE OPERADORES

---
+++ 3.operadores/ejercicios.js
@@ -0,0 +1,27 @@
+//EJEMPLOS DE TIPOS DE OPERADORES
+//OPERADORES ARITMETICOS
+let suma 
+suma=(1+2)
+console.log(suma)
+//OPERADORES LOGICOS 
+let manzana_fruta
+let cebolla_fruta
+manzana_fruta=true
+cebolla_fruta=false
+console.log(manzana_fruta)
+//OPERADORES DE COMPARACION 
+let igual
+let mayor
+let menor
+mayor=(5>4)
+igual=(2==2)
+menor=(10<5)
+console.log(menor)
+//OPERDORES DE BINARIOS
+let binario
+binario=(-9 >>> 2)
+console.log(binario)
+//OPERDORES DE ASIGNACION 
+let miSaludo
+miSaludo="hola como estas"
+console.log(miSaludo)
